293773e622066a9ee8dd12b6fd600c9666498dcf
Anand-Thakar <anandthakar58@gmail.com>
2023-04-23 23:53:03 -0400

    first commit

---
+++ src/main/java/com/sdm/hw/ivr/IvrRouterApplication.java
@@ -0,0 +1,13 @@
+package com.sdm.hw.ivr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvrRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvrRouterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sdm/hw/ivr/Main.java
@@ -0,0 +1,18 @@
+package com.sdm.hw.ivr;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String s = stringBuilder.append(StringUtils.rightPad("200", 7, " "))
+                .append(StringUtils.leftPad("1234567", 10, " "))
+                .append(StringUtils.rightPad(" ", 7))
+                .append(StringUtils.rightPad(" ", 4))
+                .append("\n").toString();
+
+        System.out.println(s);
+    }
+}
